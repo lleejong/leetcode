9398b1c886eb035d6b336b8ac8dcb7e8ef8c8570
lleejong <lleejong@gmail.com>
2019-04-07 14:33:40 +0900

    Upload Problem #13 solution

--- 0013_roman_to_integer/solution.py
+++ 0013_roman_to_integer/solution.py
@@ -10,24 +10,17 @@ class Solution:
         self.roman_int_map['D'] = 500
         self.roman_int_map['M'] = 1000
 
-
     def romanToInt(self, s: str) -> int:
         sum = 0
-        prev_char = ''
-        # char_arr = list(s).reverse()
-        # for i in range(0, len(char_arr)):
-        #     current_int = self.roman_int_map[list[i]]
-        #     j = i + 1
 
-        for i in range(0, len(s)):
-            current_int = self.roman_int_map[s[i]]
-            # if current_int > sum:
-            #     sum = current_int - sum
-            # else:
-            #     sum += current_int
-            prev_char = s[i]
+        for i in range(0, len(s) - 1):
+            if self.roman_int_map[s[i]] >= self.roman_int_map[s[i + 1]]:
+                sum += self.roman_int_map[s[i]]
+            else:
+                sum -= self.roman_int_map[s[i]]
+
+        sum += self.roman_int_map[s[len(s) - 1]]
         return sum
-
 
 sol = Solution()
 print(sol.romanToInt("LVIII"))
