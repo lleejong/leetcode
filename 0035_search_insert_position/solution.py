f0932b41a03b210dccf0a07feea2f83c8adc1e8e
lleejong <lleejong@gmail.com>
2019-04-12 12:11:23 +0900

    upload #35 solution

---
+++ 0035_search_insert_position/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return 0
+        for i in range(0, len(nums)):
+            if target <= nums[i]:
+                return i
+
+        return len(nums)
+
+sol = Solution()
+print(sol.searchInsert([1,3,5,6], 5))
+print(sol.searchInsert([1,3,5,6], 2))
+print(sol.searchInsert([1,3,5,6], 7))
+print(sol.searchInsert([1,3,5,6], 0))
